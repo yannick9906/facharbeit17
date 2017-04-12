7495d3991c60e6b13e98af615d647600a44bcf7a
yannick9906 <yannick.felix1999@gmail.com>
2017-04-12 23:05:37 +0200

    + Development Base

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yanni on 2017-04-12.
+ */
+
+$(document).ready(function() {
+    $(".dropdown-button").dropdown();
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+
+    //Load name from database
+});
